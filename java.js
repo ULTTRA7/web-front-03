ac5a42d439adaf57e560239748fcb1d4229b21ce
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:47:54 -0300

    adicioando mais um arquivo

--- java.js
+++ java.js
@@ -1,1 +1,16 @@
-alert("Olá mundo!")
+//criando um alert em JavaScript
+alert("Olá mundo!");
+
+/* 
+espaço pra comentar
+*/
+
+//Adicionando uma mensagem na tela
+document.write('Olá mundo!');
+
+//adicionando console.log
+console.log("Olá sou o victor!");
+
+//Variaveis
+let mensagem = "Olá Mundo!";
+console.log("Mensagem de variavel:", mensagem);
